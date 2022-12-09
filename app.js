8a65db8a8c28f67d58f080e128c82306941bd1f2
KatsuroAlex <katsuroay@mail.ru>
2022-12-09 20:31:56 +0400

    bug fix

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const bodyParser = require('body-parser');
 const { PORT = 3000, BASE_PATH } = process.env;
 const app = express();
 
+app.use(bodyParser.json());
+
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
@@ -18,8 +20,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 // подключаем мидлвары, роуты и тд
-
-app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   req.user = { _id: '638a18e5564e527e4b5bd599' }; // вставьте сюда _id созданного в предыдущем пункте пользователя
