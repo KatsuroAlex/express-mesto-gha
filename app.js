f9905b0ce263890d407a47a07170e62f83af8367
KatsuroAlex <katsuroay@mail.ru>
2022-12-12 22:13:01 +0400

    bugfix

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const app = express();
 // // подключаем мидлвары, роуты и тд
 app.use(bodyParser.json());
 
+/// основные роуты
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.use('*', (req, res) => res.status(404).send({ message: 'Указан неправильный путь' }));
+
 // подключаемся к серверу mongo
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
@@ -24,9 +30,3 @@ app.use((req, res, next) => {
   req.user = { _id: '638a18e5564e527e4b5bd599' }; // вставьте сюда _id созданного в предыдущем пункте пользователя
   next();
 });
-
-/// основные роуты
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
-
-app.use('*', (req, res) => res.status(404).send({ message: 'Указан неправильный путь' }));
