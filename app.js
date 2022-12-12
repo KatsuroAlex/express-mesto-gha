d6ac0056a7df39a83663baf0298b0c339e3146d2
KatsuroAlex <katsuroay@mail.ru>
2022-12-12 23:22:07 +0400

    project complete 12.12.2022 23:22 second iteration

--- app.js
+++ app.js
@@ -16,17 +16,12 @@ app.use((req, res, next) => {
 
 /// основные роуты
 app.use(router);
-// app.use('/users', require('./routes/users'));
-// app.use('/cards', require('./routes/cards'));
 
 app.use('*', (req, res) => res.status(404).send({ message: 'Указан неправильный путь' }));
 
 // подключаемся к серверу mongo
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  // useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
 }, () => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
