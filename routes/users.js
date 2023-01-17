5654080ef5f70ac1a5d7bf1daf0844de12d827dd
KatsuroAlex <katsuroay@mail.ru>
2023-01-18 00:04:05 +0400

    test

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
-const validator = require('validator');
+const { validateUser, validateUserProfile, validateUserAvatar } = require('../middlewares/validation');
 
 const {
   getUsers, getUser, updateUser, updateAvatar, findUser,
@@ -27,28 +27,28 @@ const {
 
 router.get('/', getUsers); // возвращает всех пользователей
 
-// router.get('/:id', validateUser, getUser); // возвращает пользователя по ID
-router.get('/:id', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().alphanum().required().length(24),
-  }),
-}), getUser);
+router.get('/:id', validateUser, getUser); // возвращает пользователя по ID
+// router.get('/:id', celebrate({
+//   params: Joi.object().keys({
+//     userId: Joi.string().alphanum().required().length(24),
+//   }),
+// }), getUser);
 
-// router.patch('/me', validateUserProfile, updateUser);
-router.get('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-  }),
-}), updateUser);
+router.patch('/me', validateUserProfile, updateUser);
+// router.get('/me', celebrate({
+//   body: Joi.object().keys({
+//     name: Joi.string().required().min(2).max(30),
+//     about: Joi.string().required().min(2).max(30),
+//   }),
+// }), updateUser);
 
-// router.patch('/me/avatar', validateUserAvatar, updateAvatar);
-router.patch('/me/avatar', celebrate({
-  body: Joi.string().custom((value, helpers) => {
-    if (validator.isURL(value)) return value;
-    return helpers.message('Неверный формат ссылки на изображение');
-  }),
-}), updateAvatar);
+router.patch('/me/avatar', validateUserAvatar, updateAvatar);
+// router.patch('/me/avatar', celebrate({
+//   body: Joi.string().custom((value, helpers) => {
+//     if (validator.isURL(value)) return value;
+//     return helpers.message('Неверный формат ссылки на изображение');
+//   }),
+// }), updateAvatar);
 
 router.get('/me', findUser); // найти пользователя
 
