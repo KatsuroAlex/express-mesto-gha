2b2844f3760cfd51c91585ffe258adae3b3dd0b2
KatsuroAlex <katsuroay@mail.ru>
2023-01-18 16:05:22 +0400

    test

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
 
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
@@ -6,7 +8,17 @@ const {
 const { validateCard, validateCreateCard } = require('../middlewares/validation');
 
 router.get('/', getCards); // возвращает все карточки
-router.post('/', validateCreateCard, createCard); // создает карточку
+// router.post('/', validateCreateCard, createCard); // создает карточку
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().custom((value, helpers) => {
+      if (validator.isURL(value)) return value;
+      return helpers.message('Неверный формат ссылки на изображение');
+    }),
+  }),
+}), createCard);
+
 router.delete('/:cardId', validateCard, deleteCard); // удаляет карточку по ID
 router.put('/:cardId/likes', validateCard, likeCard); // поставить лайк на карточку
 router.delete('/:cardId/likes', validateCard, dislikeCard);
